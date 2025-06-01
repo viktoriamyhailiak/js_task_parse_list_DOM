c8b1f2502d3c2482241c54226a7000d02a5da021
Viktoriia <viktoriamyhailiak@gmail.com>
2025-06-01 17:50:13 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-// write code here
+let list = document.getElementsByTagName('li');
+
+function sortList(x) {
+  const array = [...x];
+
+  for (const n of array) {
+    n.dataset.salary = +n.dataset.salary
+      .split('')
+      .filter((elem) => !isNaN(elem))
+      .join('');
+  }
+
+  array.sort((z, y) => y.dataset.salary - z.dataset.salary);
+  list = array;
+}
+
+function getEmployees(x) {
+  const sortedUl = document.createElement('ul');
+
+  document.body.append(sortedUl);
+
+  for (let y = 0; y < x.length; y++) {
+    sortedUl.append(x[y]);
+  }
+
+  document.body.querySelector('ul').remove();
+}
+
+sortList(list);
+getEmployees(list);
